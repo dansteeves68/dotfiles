e53e1113a0be0ba76a484771b145aa4570ff2b03
Dan Steeves <dan@thesteeves.org>
2022-08-11 18:03:10 -0500

    Nixfmt

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,15 +1,11 @@
 { config, pkgs, ... }:
 let
-  python-with-global-packages = pkgs.python3.withPackages (
-    ps: with ps; [ 
-      pandas
-      pytest
-    ]
-  );
+  python-with-global-packages =
+    pkgs.python3.withPackages (ps: with ps; [ pandas pytest ]);
   user = builtins.getEnv "USER";
-  emailAddress = if user == "dan" then "dan@thesteeves.org" else "dan.steeves@thrivent.com";
-in
-{
+  emailAddress =
+    if user == "dan" then "dan@thesteeves.org" else "dan.steeves@thrivent.com";
+in {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [ python-with-global-packages ];
@@ -23,7 +19,7 @@ in
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
@@ -54,9 +50,9 @@ in
       "1password-cli"
       "discord"
       "iterm2"
-      "marked" 
+      "marked"
       "netnewswire"
-      "sublime-text" 
+      "sublime-text"
       "tower"
     ];
     cleanup = "none";
@@ -68,12 +64,12 @@ in
   imports = [ <home-manager/nix-darwin> ];
 
   users.users.${user} = {
-      name = user;
-      home = "/Users/${user}";
-    };
+    name = user;
+    home = "/Users/${user}";
+  };
 
   home-manager.users.${user} = { pkgs, ... }: {
-    home.packages = with pkgs; [ 
+    home.packages = with pkgs; [
       awscli2
       black
       dotfiles
@@ -83,9 +79,8 @@ in
       ripgrep
     ];
 
-    home.sessionPath = [
-      "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-    ];
+    home.sessionPath =
+      [ "/Applications/Sublime Text.app/Contents/SharedSupport/bin" ];
 
     home.stateVersion = "22.05";
 
