5f45bc2232146478e72d12239eec26abc66f9e74
Dan Steeves <dan@thesteeves.org>
2022-08-17 19:26:04 -0500

    More settings

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -29,6 +29,8 @@ in {
     casks = [
       "1password"
       "1password-cli"
+      "cardhop"
+      "fantastical"
       # "discord"
       "iterm2"
       "marked"
@@ -44,20 +46,34 @@ in {
   # networking.computerName = ;
 
   system.defaults.dock.autohide = true;
+  system.defaults.dock.autohide-delay = "0.5";
+  system.defaults.dock.autohide-time-modifier = "1.0";
+  system.defaults.dock.launchanim = false;
+  system.defaults.dock.mineffect = null;
   system.defaults.dock.show-process-indicators = true;
   system.defaults.dock.show-recents = false;
-  system.defaults.dock.static-only = true;
+  system.defaults.dock.static-only = false;
+  system.defaults.dock.wvous-tl-corner = 4;
 
   system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.AppleShowAllFiles = true;
+  system.defaults.finder.CreateDesktop = false; # Whether to show icons on the desktop or not.
   system.defaults.finder.ShowPathbar = true;
+  system.defaults.finder.ShowStatusBar = false;
+  system.defaults.finder.QuitMenuItem = true;
+
+  system.defaults.screencapture.location = "~/Screenshots/";
 
   # TODO system.defaults.NSGlobalDomain.com.apple.mouse.tapBehavior = 1; ### broken in Monterey
 
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
 
+  # system.nixpkgsRelease = ;
+
   users.users.${user} = {
     name = user;
+    description = "Dan Steeves";
     home = "/Users/${user}";
   };
 
@@ -66,7 +82,7 @@ in {
   #
   imports = [ <home-manager/nix-darwin> ];
   home-manager.users.${user} = { pkgs, ... }: {
-    home.packages = with pkgs; [ awscli2 black nixfmt pandoc ripgrep ];
+    home.packages = with pkgs; [ awscli2 black m-cli nixfmt pandoc ripgrep somafm-cli ];
     home.sessionPath =
       [ "/Applications/Sublime Text.app/Contents/SharedSupport/bin" ];
     home.stateVersion = "22.05";
