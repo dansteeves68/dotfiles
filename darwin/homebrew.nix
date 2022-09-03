ecee41339c93edd308ffa2446bd42e29a2569b23
Dan Steeves <dan@thesteeves.org>
2022-09-03 13:41:23 -0500

    add stuff and alfred changes

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -44,36 +44,37 @@ in {
   # Commented apps suffer continual update issue:
   # https://github.com/malob/nixpkgs/issues/9
   homebrew.masApps = {
+    # "1Password for Safari" = 1569813296;
+    "Numbers" = 409203825;
+    "Pins for Pinboard" = 1547106997;
     # "1Blocker" = 1365531024;
     # "1Password" = 1333542190;
-    # "1Password for Safari" = 1569813296;
     # "Accelerate for Safari" = 1459809092;
     # "Apple Configurator 2" = 1037126344;
-    # DaisyDisk = 411643860;
     # "Dark Mode for Safari" = 1397180934;
-    # Deliveries = 290986013;
-    # Fantastical = 975937182;
     # "Gemini 2" = 1090488118;
     # "iMazing Profile Editor" = 1487860882;
-    # Keynote = 409183694;
     # "LG Screen Manager" = 1142051783;
-    # MindNode = 1289197285;
-    Numbers = 409203825;
-    # Pages = 409201541;
-    # Patterns = 429449079;
     # "Pixelmator Classic" = 407963104;
     # "Pixelmator Pro" = 1289583905;
     # "Save to Raindrop.io" = 1549370672;
-    # Slack = 803453959;
-    # SiteSucker = 442168834;
     # "Things 3" = 904280696;
+    # "WiFi Explorer" = 494803304;
+    # "Yubico Authenticator" = 1497506650;
+    # DaisyDisk = 411643860;
+    # Deliveries = 290986013;
+    # Fantastical = 975937182;
+    # Keynote = 409183694;
+    # MindNode = 1289197285;
+    # Pages = 409201541;
+    # Patterns = 429449079;
+    # SiteSucker = 442168834;
+    # Slack = 803453959;
+    # TODO: Re-enable once macOS Ventura officially launches
     # TripMode = 1513400665;
     # Ulysses = 1225570693;
     # Vimari = 1480933944;
-    # "WiFi Explorer" = 494803304;
-    # TODO: Re-enable once macOS Ventura officially launches
     # Xcode = 497799835;
-    # "Yubico Authenticator" = 1497506650;
   };
 
   # If an app isn't available in the Mac App Store, or the version in the App Store has
