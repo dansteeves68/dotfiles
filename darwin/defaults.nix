c77bb5e2b42bd816ca9f3f635c97a9fcc87f5594
Dan Steeves <dan@thesteeves.org>
2022-09-06 18:51:17 -0500

    remove unsupported trackpad scaling config

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -77,6 +77,4 @@
     Clicking = true; # true to enable tap-to-click
     TrackpadRightClick = true;
   };
-
-  system.defaults.NSGlobalDomain.com.apple.trackpad.scaling = 2.0; # 0 to 3 for trackpad speed
 }
