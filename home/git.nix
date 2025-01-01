2bbc835ff3b682b953342c8018ab1c0e4cb9d0c3
Dan Steeves <dan@thesteeves.org>
2025-01-01 09:05:11 -0600

    A bit of git and gh

--- home/git.nix
+++ home/git.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  programs.gh.enable = true;
+
   programs.git = {
     enable = true;
+    extraConfig = {
+      push.autoSetupRemote = true;
+    };
     ignores = [ ".DS_Store" ];
     userEmail = config.home.user-info.email;
     userName = config.home.user-info.fullName;
